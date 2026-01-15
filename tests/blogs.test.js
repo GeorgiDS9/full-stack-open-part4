c746cb8a0108d931bde7d09314d815a68b05bb07
georgiana <gchiriac2012@gmail.com>
2026-01-15 10:29:57 +0100

    E.g. 4.4.

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -1,10 +1,28 @@
 const { test, describe } = require("node:test");
 const assert = require("node:assert");
 const listHelper = require("../utils/list_helper");
+const { listWithOneBlog, blogs } = require("./test_data");
 
 test("dummy returns one", () => {
   const blogs = [];
 
   const result = listHelper.dummy(blogs);
   assert.strictEqual(result, 1);
+});
+
+describe("total likes", () => {
+  test("of empty list is zero", () => {
+    const result = listHelper.totalLikes([]);
+    assert.strictEqual(result, 0);
+  });
+
+  test("when list has only one blog, equals blog's likes", () => {
+    const result = listHelper.totalLikes([blogs[0]]);
+    assert.strictEqual(result, 7);
+  });
+
+  test("of a bigger list is calculated right", () => {
+    const result = listHelper.totalLikes(blogs);
+    assert.strictEqual(result, 36);
+  });
 });
