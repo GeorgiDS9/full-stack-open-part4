5800439e21e86299ae15dfa6789747f04a656b29
georgiana <gchiriac2012@gmail.com>
2026-01-22 19:48:09 +0100

    Ex 4.6

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -25,4 +25,39 @@ describe("total likes", () => {
     const result = listHelper.totalLikes(blogs);
     assert.strictEqual(result, 36);
   });
+
+  describe("most blogs", () => {
+    test("of empty list is null", () => {
+      const result = listHelper.mostBlogs([]);
+      assert.strictEqual(result, null);
+    });
+
+    test("when list has only one blog, returns that author with count 1", () => {
+      const result = listHelper.mostBlogs([blogs[0]]);
+      assert.deepStrictEqual(result, {
+        author: "Michael Chan",
+        blogs: 1,
+      });
+    });
+
+    test("when list has multiple blogs, returns author with most blogs", () => {
+      const result = listHelper.mostBlogs(blogs);
+      assert.deepStrictEqual(result, {
+        author: "Robert C. Martin",
+        blogs: 3,
+      });
+    });
+
+    test("when multiple authors have same count, returns any one of them", () => {
+      const testBlogs = [
+        { author: "Author A", title: "Blog 1", likes: 1 },
+        { author: "Author B", title: "Blog 2", likes: 2 },
+        { author: "Author A", title: "Blog 3", likes: 3 },
+        { author: "Author B", title: "Blog 4", likes: 4 },
+      ];
+      const result = listHelper.mostBlogs(testBlogs);
+      assert.strictEqual(result.blogs, 2);
+      assert.ok(result.author === "Author A" || result.author === "Author B");
+    });
+  });
 });
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -16,8 +16,31 @@ const favoriteBlog = (blogs) => {
   }, blogs[0]);
 };
 
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) {
+    return null;
+  }
+
+  // Count blogs per author
+  const authorCounts = blogs.reduce((counts, blog) => {
+    counts[blog.author] = (counts[blog.author] || 0) + 1;
+    return counts;
+  }, {});
+
+  // Find the author with the most blogs
+  const topAuthor = Object.keys(authorCounts).reduce((max, author) => {
+    return authorCounts[author] > authorCounts[max] ? author : max;
+  });
+
+  return {
+    author: topAuthor,
+    blogs: authorCounts[topAuthor],
+  };
+};
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
 };
